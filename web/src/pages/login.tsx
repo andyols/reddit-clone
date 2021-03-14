48b5770a8e5da2acb2b9cd8ac23ffc9d66a7b881
andyols <andyols.dev@gmail.com>
2021-03-14 12:38:58 -0500

    Login form / mutation - followed the same workflow as register functionality - reused the bulk majority of register.tsx in login.tsx... neat!

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,52 @@
+import { Button, Stack } from '@chakra-ui/react'
+import { Form, Formik } from 'formik'
+import { useRouter } from 'next/dist/client/router'
+import React from 'react'
+import { InputField } from '../components/InputField'
+import { Wrapper } from '../components/Wrapper'
+import { useLoginMutation } from '../generated/graphql'
+import { toErrorMap } from '../utils/toErrorMap'
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter()
+  const [, login] = useLoginMutation()
+
+  return (
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values })
+
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors))
+          } else if (response.data?.login.user) {
+            // yay, it worked
+            router.push('/')
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Stack as={Form} spacing={4}>
+            <InputField
+              name='username'
+              placeholder='username'
+              label='Username'
+            />
+            <InputField
+              name='password'
+              placeholder='password'
+              label='Password'
+              type='password'
+            />
+            <Button type='submit' colorScheme='teal' isLoading={isSubmitting}>
+              Login
+            </Button>
+          </Stack>
+        )}
+      </Formik>
+    </Wrapper>
+  )
+}
+
+export default Login
